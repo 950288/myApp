044b1f06a4978a95b040ddc575257c84694ba819
950288 <950288s@gmail.com>
2023-09-28 10:53:03 +0800

    update 2 files, create 15 files and delete 1 file

---
+++ maze/questions.js
@@ -0,0 +1,232 @@
+export const QUESTIONS = [
+  {
+    Q: 'What are plastic bottles typically made of?',
+    A: 'PET',
+    B: 'PE',
+    C: 'PVC',
+    D: 'PP',
+    correct: 'PET'
+  },
+  {
+    Q: 'How can you reduce plastic waste at home?',
+    A: "Don't care about the amount of plastic waste",
+    B: 'Use reusable water bottles and cups',
+    C: 'Litter plastic waste',
+    correct: 'Use reusable water bottles and cups'
+  },
+  {
+    Q: 'Do you think using reusable shopping bags is better than using single-use plastic bags when shopping?',
+    A: 'Yes, because reusable bags are more durable',
+    B: 'Not necessarily, because single-use bags are more convenient',
+    C: "I don't care, because bags are all the same",
+    correct: 'Yes, because reusable bags are more durable'
+  },
+  {
+    Q: 'Do you know how to properly dispose of electronic waste?',
+    A: 'Throw it in the trash',
+    B: 'Place it in special electronic waste recycling bins',
+    C: 'Sell it to someone else',
+    D: 'Bury it underground',
+    correct: 'Place it in special electronic waste recycling bins'
+  },
+  {
+    Q: 'What can you do at home to reduce energy consumption and environmental pollution?',
+    A: 'Wash clothes twice a day',
+    B: 'Keep the TV on for long hours',
+    C: 'Turn off lights and appliances when not in use',
+    D: "Don't care about energy consumption",
+    correct: 'Turn off lights and appliances when not in use'
+  },
+  {
+    Q: 'How can you reduce water waste at home?',
+    A: 'Waste water recklessly',
+    B: 'Turn off the tap when not needed',
+    C: 'Take long showers',
+    D: 'Take no measures to reduce water waste',
+    correct: 'Turn off the tap when not needed'
+  },
+  {
+    Q: 'Are you aware that recycling old phones and electronic devices can reduce electronic waste?',
+    A: 'Yes, I am aware',
+    B: 'No, I dispose of old electronic devices randomly',
+    C: "I don't care",
+    correct: 'Yes, I am aware'
+  },
+  {
+    Q: 'Why should we recycle plastic bottles?',
+    A: 'Because they can turn into magic potions',
+    B: 'Because recycling helps reduce waste',
+    C: "Because they become heavier",
+    correct: 'Because recycling helps reduce waste'
+  },
+  {
+    Q: 'What is the primary impact of plastic waste on marine ecosystems?',
+    A: 'Increases fish spawning grounds',
+    B: 'Provides a food supply',
+    C: "Causes marine pollution and wildlife ingestion",
+    D: 'Promotes coral reef growth',
+    correct: 'Causes marine pollution and wildlife ingestion'
+  },
+  {
+    Q: 'How long does it take for plastic to degrade?',
+    A: 'Several years',
+    B: 'Several decades',
+    C: "Several centuries",
+    D: 'Several millennia',
+    correct: 'Several centuries'
+  },
+  {
+    Q: 'What is the primary source of microplastics?',
+    A: 'Emissions from plastic production facilities',
+    B: 'Waste from large commercial ships',
+    C: "Decomposition of plastic waste",
+    D: 'Food packaging',
+    correct: 'Decomposition of plastic waste'
+  },
+  {
+    Q: 'Which ocean region is referred to as the "plastic pollution vortex"?',
+    A: 'Caribbean Sea',
+    B: 'Arctic Ocean',
+    C: "Pacific Garbage Patch",
+    D: 'Indian Ocean',
+    correct: 'Pacific Garbage Patch'
+  },
+  {
+    Q: 'What is the most common pathway for microplastics to enter the food chain?',
+    A: 'Ingestion from rivers and lakes',
+    B: 'Ingestion by marine organisms near coastlines',
+    C: "Atmospheric deposition",
+    D: 'Drifting from desert storms',
+    correct: 'Ingestion by marine organisms near coastlines'
+  },
+  {
+    Q: 'What is one of the major threats of plastic pollution to marine life?',
+    A: 'Fish migration',
+    B: 'Adaptive evolution',
+    C: "Bird migration in the oceans",
+    D: 'Ingestion and entanglement',
+    correct: 'Ingestion and entanglement'
+  },
+  {
+    Q: 'Which type of plastic product is typically not recyclable?',
+    A: 'Plastic bottles',
+    B: 'Plastic bags',
+    C: "Plastic containers",
+    D: 'Plastic toys',
+    correct: 'Plastic bags'
+  },
+  {
+    Q: 'Which international agreement aims to reduce the impact of plastic waste on the marine environment?',
+    A: 'Kyoto Protocol',
+    B: 'Paris Agreement',
+    C: "Montreal Protocol",
+    D: 'Basel Convention',
+    correct: 'Basel Convention'
+  },
+  {
+    Q: 'Why is it important not to litter plastic waste?',
+    A: 'Because plastic makes the Earth sick',
+    B: 'Because littering is a bad habit',
+    C: "Because litter flies away",
+    correct: 'Because plastic makes the Earth sick'
+  },
+  {
+    Q: 'What can plastic waste be turned into?',
+    A: 'Magic flying machines',
+    B: 'New toys',
+    C: "Recycled plastic products",
+    correct: 'Recycled plastic products'
+  },
+  {
+    Q: 'Why should we recycle plastic bottles?',
+    A: 'Because they can turn into magic potions',
+    B: 'Because recycling helps reduce waste',
+    C: "Because they become heavier",
+    correct: 'Because recycling helps reduce waste'
+  },
+  {
+    Q: 'Do you believe environmental protection is for the current generation or for future generations?',
+    A: 'For the current generation',
+    B: 'For future generations',
+    C: "I'm not sure",
+    correct: 'For the current generation'
+  },
+  {
+    Q: 'What is the primary impact of plastic pollution on the environment?',
+    A: 'Air pollution',
+    B: 'Soil pollution',
+    C: "Water pollution",
+    D: "All of the above",
+    correct: 'All of the above'
+  },
+  {
+    Q: 'How does plastic pollution affect wildlife?',
+    A: 'Poisoning in the food chain',
+    B: 'Habitat destruction',
+    C: "Disruption of predator-prey relationships",
+    D: "All of the above",
+    correct: 'All of the above'
+  },
+  {
+    Q: 'What is the most prevalent type of plastic in plastic waste?',
+    A: 'Plastic bags',
+    B: 'Plastic bottles',
+    C: "Plastic containers",
+    D: "Plastic packaging materials",
+    correct: 'Plastic bottles'
+  },
+  {
+    Q: 'Why are disposable plastic products considered the primary contributors to plastic pollution?',
+    A: 'Easy to discard',
+    B: 'Difficult to degrade',
+    C: "Extensive use",
+    D: "All of the above",
+    correct: 'All of the above'
+  },
+  {
+    Q: 'What are "microplastics"?',
+    A: 'Plastic bags',
+    B: 'Extremely small plastic particles',
+    C: "Plastic bottles",
+    D: "Plastic containers",
+    correct: 'Extremely small plastic particles'
+  },
+  {
+    Q: 'What is the most noticeable impact of plastic pollution on marine ecosystems?',
+    A: 'Harm to marine birds',
+    B: 'Harm to marine mammals',
+    C: "Damage to coral reefs",
+    D: "All of the above",
+    correct: 'All of the above'
+  },
+  {
+    Q: 'How does plastic pollution affect human health?',
+    A: 'Causes chronic diseases',
+    B: 'Consumption of contaminated seafood',
+    C: "Affects air quality",
+    D: "All of the above",
+    correct: 'All of the above'
+  },
+  {
+    Q: 'What do plastic waste ultimately decompose into?',
+    A: 'Petroleum',
+    B: 'Water',
+    C: "Organic substances",
+    D: "Metals",
+    correct: 'Organic substances'
+  },
+  {
+    Q: 'Are there known plastic-degrading enzymes in the world?',
+    A: 'Yes',
+    B: 'No',
+    correct: 'Yes'
+  },
+  {
+    Q: 'What is the primary source of "plastic pollution"?',
+    A: 'Industrial waste',
+    B: 'Urban garbage',
+    C: "Medical waste",
+    D: "All of the above",
+    correct: 'All of the above'
+  }
+]
